1f385cfa5bcd86a504ce64dea4574f035c5f122c
MateoRocha475 <matrocha@ucdavis.edu>
2025-11-24 06:52:51 +0000

    q16 test 2

--- part1.py
+++ part1.py
@@ -495,7 +495,7 @@ Write three functions, a, b, and c that use different levels of parallelism.
 
 def q16_a():
     # For this one, create the RDD yourself. Choose the number of partitions.
-    rdd = sc.parallelize(range(1: 1000), 2)
+    rdd = sc.parallelize(range(1000), 2)
     get_key = rdd.map(lambda x: ("", x))
     get_map = general_map(get_key, lambda k1, v1: [(0, v1)])
     def subtraction(a, b):
@@ -505,7 +505,7 @@ def q16_a():
 
 def q16_b():
     # For this one, create the RDD yourself. Choose the number of partitions.
-    rdd = sc.parallelize(range(1: 1000), 8)
+    rdd = sc.parallelize(range(1000), 8)
     get_key = rdd.map(lambda x: ("", x))
     get_map = general_map(get_key, lambda k1, v1: [(0, v1)])
     def subtraction(a, b):
@@ -515,7 +515,7 @@ def q16_b():
 
 def q16_c():
     # For this one, create the RDD yourself. Choose the number of partitions.
-    rdd = sc.parallelize(range(1: 1000), 13)
+    rdd = sc.parallelize(range(1000), 13)
     get_key = rdd.map(lambda x: ("", x))
     get_map = general_map(get_key, lambda k1, v1: [(0, v1)])
     def subtraction(a, b):
